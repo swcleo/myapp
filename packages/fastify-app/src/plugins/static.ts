ece36ba07d8631176342a9423d82e88c333d3092
leo_chen <leo_chen@mail.chungyo.net>
2022-11-08 16:57:50 +0800

    feat: add vue example

--- packages/fastify-app/src/plugins/static.ts
+++ packages/fastify-app/src/plugins/static.ts
@@ -3,14 +3,23 @@ import FastifyStatic from "@fastify/static";
 import path from "path";
 
 export default fp(async function (fastify) {
+  // app
   fastify.register(FastifyStatic, {
     root: path.join(__dirname, "../public"),
     prefix: "/public/",
   });
 
+  // angular
   fastify.register(FastifyStatic, {
     root: path.join(__dirname, "../angular"),
     prefix: "/angular/",
+    decorateReply: false,
+  });
+
+  // vue
+  fastify.register(FastifyStatic, {
+    root: path.join(__dirname, "../vue"),
+    prefix: "/vue/",
     decorateReply: false,
   });
 });
---
+++ packages/fastify-app/src/routes/vue/index.ts
@@ -0,0 +1,8 @@
+import { FastifyInstance } from "fastify";
+import path from "path";
+
+export default async function (fastify: FastifyInstance) {
+  fastify.get("/", (req, reply) => {
+    reply.sendFile("index.html", path.join(__dirname, "../../vue"));
+  });
+}
